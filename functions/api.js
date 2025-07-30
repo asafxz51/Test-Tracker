f8cca9f288c1a8db58baacbe4e240078e3682a21
asafxz51 <asafxz51@gmail.com>
2025-07-30 17:21:52 +0300

    serverless fix

--- functions/api.js
+++ functions/api.js
@@ -1,5 +1,33 @@
-const serverless = require('serverless-http');
-const app = require('../server'); // Adjust path if your main app file is named differently
+// functions/api.js
 
-// We are exporting the express app wrapped in the serverless package.
-module.exports.handler = serverless(app); 
+const serverless = require('serverless-http');
+const mongoose = require('mongoose');
+const app = require('../server'); // Your main express app
+
+// This is the variable that will hold our connection
+let cachedDb = null;
+
+const connectToDatabase = async () => {
+ // If we already have a connection, reuse it
+ if (cachedDb) {
+  console.log('=> using cached database instance');
+  return cachedDb;
+ }
+
+ // If not, create a new connection
+ console.log('=> using new database instance');
+ const db = await mongoose.connect(process.env.MONGODB_URI);
+ cachedDb = db;
+ return db;
+};
+
+// This is our main handler function
+const handler = async (req, res) => {
+ // Ensure we have a database connection before handling the request
+ await connectToDatabase();
+ // This uses the serverless-http wrapper to run your Express app
+ const expressHandler = serverless(app);
+ return expressHandler(req, res);
+};
+
+module.exports.handler = handler;
